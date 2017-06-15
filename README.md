84a82f995229754ef6b19275c5ead8810d026f82
LAPTOP-13RBRGUG\邓清 <1030127004@qq.com>
2017-06-15 16:05:16 +0800

    no message

--- README.md
+++
@@ -1,2 +0,0 @@
-# paopaotang
-A project that we will make in next two months.
---
+++ collidable.h
@@ -0,0 +1,25 @@
+#include "cocos2d.h"
+USING_NS_CC;
+
+class Collidable
+{	
+public:
+	//Åö×²²ã
+	TMXLayer* meta;
+	//Åö×²º¬ÒÆ¶¯²¹È«
+	bool collidable_up(int x, int y, int speed);
+	bool col_move_up_left(int x, int y, int speed);
+	bool col_move_up_right(int x, int y, int speed);
+
+	bool collidable_down(int x, int y,int speed);
+	bool col_move_down_left(int x, int y, int speed);
+	bool col_move_down_right(int x, int y, int speed);
+
+	bool collidable_left(int x, int y, int speed);
+	bool col_move_left_up(int x, int y, int speed);
+	bool col_move_left_down(int x, int y, int speed);
+
+	bool collidable_right(int x, int y, int speed);
+	bool col_move_right_up(int x, int y, int speed);
+	bool col_move_right_down(int x, int y, int speed);
+};
